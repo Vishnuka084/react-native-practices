14d308b8b586540dcab02156e31d4c5b8d195050
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-04-26 09:37:05 +0530

    create loging page

--- screens/Login.js
+++ screens/Login.js
@@ -4,12 +4,15 @@ import React from 'react'
 export default function Login() {
   return (
     <View style={styles.container}>
-      <TextInput
+      <TextInput 
+        style={styles.input1}
         placeholder='Username'
       />
       <TextInput 
+        style={styles.input2}
         placeholder='Password'/>
         <Button
+            style={styles.btn}
             title="Login"
             color="#841584"
         />  
@@ -17,10 +20,27 @@ export default function Login() {
   )
 }
 
-
 const styles = StyleSheet.create({
+    input1:{
+        marginTop:'40%',
+        padding:10,
+        borderWidth:1,
+        width:'80%',
+    },
+    input2:{
+        marginTop:'2%',
+        padding:10,
+        borderWidth:1,
+        width:'80%',
+    },
     container:{
-        flex:1
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    btn:{
+        width:'60%',
+        color:'green',
+        padding:5
     }
     
 });
