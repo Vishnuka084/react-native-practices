6cb285987dccc765dfd39911203ad2f9be589ab2
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-04-26 14:19:57 +0530

    login form

--- screens/AddData.js
+++ screens/AddData.js
@@ -1,16 +1,15 @@
 import React from 'react'
-import { Input, NativeBaseProvider,Text, VStack } from 'native-base'
+import { Button, Input, NativeBaseProvider,Text, VStack } from 'native-base'
 
 export default function AddData() {
   return (
     <NativeBaseProvider>
       <Text fontSize='3xl' bold underLine mt = "10%" ml="30%"> Save Post</Text>
         <VStack space={4} alignItems="center" mt="15%">
-            <Input mx="3" placeholder='Input' w='80%' />
-            <Input mx="3" placeholder='Input' w='80%' />
-            <Input mx="3" placeholder='Input' w='80%' />
-            <Input mx="3" placeholder='Input' w='80%' />
-            <Input mx="3" placeholder='Input' w='80%' />
+            <Input mx="3" placeholder='title' w='80%' />
+            <Input mx="3" placeholder='body' w='80%' />
+            <Input mx="3" placeholder='User Id' w='80%' />
+            <Button onPress={() => console.log("hello world")}>Click Me</Button>
 
         </VStack>
     </NativeBaseProvider>
