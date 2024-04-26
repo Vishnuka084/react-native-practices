6dd139c086363ef398dbf14cd350073cf346164a
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-04-26 13:21:37 +0530

    updated Contnent

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import { NativeBaseProvider, Box , Button} from "native-base";
+import { NativeBaseProvider, Box,TextArea , Button , Switch, VStack} from "native-base";
 
 
 export default function Home() {
   return (
     <NativeBaseProvider>
         <Box>Hello World</Box>
-        <Button size={"lg"} onPress={() => console.log("hello world")}>Click Me</Button>
+        <VStack space={4} alignItems="center">
+            <Button mt={'40%'}  size="md" variant="outline" colorScheme="secondary" width={'100%'}>
+                Click me 
+            </Button>
+            <Switch defaultIsChecked colorScheme="primary" size="sm" />
+            <TextArea h={20} placeholder="Text Area Placeholder" w="75%" maxW="300" />
+        
+        </VStack>
+        
     </NativeBaseProvider>
   )
 }
